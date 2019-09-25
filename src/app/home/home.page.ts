c36d46c99918a5638b63e68e9d1a9a1d0e20aaf3
mibzman <sam@hungerperks.com>
2019-09-25 18:01:05 -0400

    got it to not catch fire

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Platform } from "@ionic/angular";
+import {
+	DocumentViewer,
+	DocumentViewerOptions
+} from "@ionic-native/document-viewer/ngx";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+	selector: "app-home",
+	templateUrl: "home.page.html",
+	styleUrls: ["home.page.scss"]
 })
 export class HomePage {
+	constructor(private platform: Platform, private document: DocumentViewer) {}
 
-  constructor() {}
+	openLocalPdf() {
+		console.log("hit the function");
 
+		let baseUrl = "file:///android_asset/www";
+		console.log("baseUrl: ", baseUrl);
+		if (this.platform.is("ios")) {
+			baseUrl = location.href.replace("/index.html", "");
+			console.log("ios baseUrl: ", baseUrl);
+		}
+		console.log("final baseUrl: ", baseUrl);
+
+		let file = "assets/test.pdf"; // baseUrl + "/assets/test.pdf";
+
+		console.log("file url: ", file);
+		let type = "application/pdf";
+		const options: DocumentViewerOptions = {
+			title: "My PDF"
+		};
+
+		this.document.canViewDocument(
+			file,
+			type,
+			options,
+			() => {
+				//onpossible
+				console.log("onpossible");
+				this.document.viewDocument(file, type, options);
+			},
+			result => {
+				//
+				console.log("on missing app", result);
+			},
+			result => {
+				//on
+				console.log("on impossible", result);
+			},
+			error => {
+				//on
+				console.log("on error", error);
+			}
+		);
+		console.log("got to the bottom");
+	}
 }
